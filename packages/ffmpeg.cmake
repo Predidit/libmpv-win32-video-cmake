d3686df1b2a8aa2d991b6a8ec7f18373645d05e8
Predidit <34627277+Predidit@users.noreply.github.com>
2024-09-30 10:57:58 +0800

    fix nvcodec

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -69,6 +69,7 @@ ExternalProject_Add(ffmpeg
         --enable-dxva2
         --enable-libmfx
         --enable-d3d11va
+        --enable-ffnvcodec
 
         --disable-vaapi
         --disable-vdpau
