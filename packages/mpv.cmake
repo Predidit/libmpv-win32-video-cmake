22fe583242a2aeadb41070dc66a5d4c3dda36583
Predidit <34627277+Predidit@users.noreply.github.com>
2024-09-30 14:14:37 +0800

    bump mpv version

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -13,8 +13,8 @@ ExternalProject_Add(mpv
         shaderc
         spirv-cross
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
-    GIT_TAG v0.36.0
     SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
         --prefix=${MINGW_INSTALL_PREFIX}
