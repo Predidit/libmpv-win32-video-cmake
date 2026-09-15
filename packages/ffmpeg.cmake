d4c8448f8e86930c26a4b3abfbb3fea715f59132
Predidit <34627277+Predidit@users.noreply.github.com>
2026-09-15 12:06:27 +0800

    build: advance ffmpeg to latest master

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -30,7 +30,7 @@ ExternalProject_Add(ffmpeg
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     SOURCE_DIR ${SOURCE_LOCATION}
     GIT_CLONE_FLAGS "--sparse --filter=tree:0"
-    GIT_TAG 6efe500d2e9e24a81bffda8825511f6bc9760cb1
+    GIT_TAG 5b9a3ad71a4b54979e7afc3a1a4269763c4f41fc
     GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
     PATCH_COMMAND ${EXEC} git apply ${CMAKE_CURRENT_SOURCE_DIR}/ffmpeg-*.patch
     UPDATE_COMMAND ""
